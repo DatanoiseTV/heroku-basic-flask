917077aeea97ab99afa0f2c2de8e23d5516b4246
Datanoise <github@ext.no-route.org>
2018-03-26 21:08:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from datetime import datetime
-import calendar
+from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    d = datetime.utcnow()
-    unixtime = calendar.timegm(d.utctimetuple())
-    return str(unixtime * 1000)
+    now = datetime.now(timezone.utc)
+    epoch = datetime(1970, 1, 1, tzinfo=timezone.utc) # use POSIX epoch
+    posix_timestamp_micros = (now - epoch) // timedelta(microseconds=1)
+    posix_timestamp_millis = posix_timestamp_micros // 1000 # or `/ 1e3` for float
+    return str(posix_timestamp_millis)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
